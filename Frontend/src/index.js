2616c3ba88a8b4a26a33492ff570f2d994ce6fda
Wahyu <safirmachsun@gmail.com>
2021-07-16 09:24:47 +0700

    Fixing index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { SidebarProvider } from "./context/SidebarContext";
-import { Windmill } from "@windmill/react-ui";
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <SidebarProvider>
-            <Windmill usePreferences>
-                <App />
-            </Windmill>
-        </SidebarProvider>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
