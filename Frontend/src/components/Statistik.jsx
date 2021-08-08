987d851f93ca8d39c5e3e434b010a3f0a2cd18b5
galihpermana29 <galihcbn123@gmail.com>
2021-08-08 15:17:53 +0700

    [galih] sizing statistik

--- Frontend/src/components/Statistik.jsx
+++ Frontend/src/components/Statistik.jsx
@@ -52,11 +52,11 @@ const Statistik = () => {
 
 	return (
 		<Sections
-			propsClass={'justify-center items-center relative'}
+			propsClass={'justify-center items-center relative min-h-info'}
 			propsClass2={'w-full'}
 		>
-			<div className="relative py-25 xs:py-10 sm:py-10 ">
-				<div className="bg-white flex xs:flex-col sm:flex-col py-15 rounded-lg xs:space-y-7">
+			<div className="relative py-20 xs:py-10 sm:py-10 ">
+				<div className="bg-white flex xs:flex-col sm:flex-col py-25 xs:py-10 rounded-lg xs:space-y-7">
 					<div
 						className="cursor-pointer flex flex-col items-center flex-grow-default"
 						onMouseEnter={() => handleMouse(13214, 283, 16, 175)}
@@ -64,10 +64,10 @@ const Statistik = () => {
 						<img
 							src={`${process.env.PUBLIC_URL}/assets/maba.png`}
 							alt="maba"
-							width="70px"
+							width="80px"
 						/>
 						<h1 className="text-2xl text-orange my-3">{count1}</h1>
-						<h1 className="text-purpleMaghrib text-lg">Mahasiswa Baru</h1>
+						<h1 className="text-purpleMaghrib text-lg ex:text-xl exl:text-xl">Mahasiswa Baru</h1>
 					</div>
 					<div
 						className="cursor-pointer flex flex-col items-center flex-grow-default"
@@ -76,10 +76,10 @@ const Statistik = () => {
 						<img
 							src={`${process.env.PUBLIC_URL}/assets/panitia.png`}
 							alt="maba"
-							width="70px"
+							width="80px"
 						/>
 						<h1 className="text-2xl text-orange my-3">{count2} </h1>
-						<h1 className="text-purpleMaghrib text-lg">Panitia RABRAW</h1>
+						<h1 className="text-purpleMaghrib text-lg ex:text-xl exl:text-xl">Panitia RABRAW</h1>
 					</div>
 					<div
 						className="cursor-pointer flex flex-col items-center flex-grow-default"
@@ -88,10 +88,10 @@ const Statistik = () => {
 						<img
 							src={`${process.env.PUBLIC_URL}/assets/fakultas.png`}
 							alt="maba"
-							width="70px"
+							width="80px"
 						/>
 						<h1 className="text-2xl text-orange my-3">{count3} </h1>
-						<h1 className="text-purpleMaghrib text-lg">Fakultas</h1>
+						<h1 className="text-purpleMaghrib text-lg ex:text-xl exl:text-xl">Fakultas</h1>
 					</div>
 					<div
 						className="cursor-pointer flex flex-col items-center flex-grow-default"
@@ -100,10 +100,10 @@ const Statistik = () => {
 						<img
 							src={`${process.env.PUBLIC_URL}/assets/prodi.png`}
 							alt="maba"
-							width="70px"
+							width="80px"
 						/>
 						<h1 className="text-2xl text-orange my-3">{count4} </h1>
-						<h1 className="text-purpleMaghrib text-lg">Program Studi</h1>
+						<h1 className="text-purpleMaghrib text-lg ">Program Studi</h1>
 					</div>
 				</div>
 			</div>
