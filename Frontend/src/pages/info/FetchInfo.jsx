7f9756457f70dc6b0f7bd82ea7658ef86577da2c
Wahyu <safirmachsun@gmail.com>
2021-08-10 22:54:23 +0700

    [machsun] Change fetch to static, change title to subtitle in FetchInfo

--- Frontend/src/pages/info/FetchInfo.jsx
+++ Frontend/src/pages/info/FetchInfo.jsx
@@ -1,62 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import Markdown from 'react-markdown';
-import InfoTemplate from './InfoTemplate';
-import { useParams } from 'react-router-dom';
-import useFetchArticles from '../../routes/useFetchArticles';
-import { useArticles } from '../../routes/Context';
-import { getArticles } from '../../routes/FetchArticles';
+import React, { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import InfoTemplate from "./InfoTemplate";
+import { useParams } from "react-router-dom";
+import useFetchArticles from "../../routes/useFetchArticles";
+import { useArticles } from "../../routes/Context";
+import { getArticles } from "../../routes/FetchArticles";
 
 const FetchInfo = () => {
-	const { slug } = useParams();
-   const [articleActive, setArticleActive] = useState(null);
-   // const [articlesFetch, setArticleFet] = useState(null);
-   const {articles, setArticles} = useArticles();
+    const { slug } = useParams();
+    const [articleActive, setArticleActive] = useState(null);
+    // const [articlesFetch, setArticleFet] = useState(null);
+    const { articles, setArticles } = useArticles();
 
-   useEffect(() => {
-      if(articles.length <= 0){
-         async function get() {
-            const res = await getArticles();
-            setArticles(res)
-         }
-         get();
+    useEffect(() => {
+        if (articles.length <= 0) {
+            async function get() {
+                const res = await getArticles();
+                setArticles(res);
+            }
+            get();
+        }
+        articles.forEach((article) => {
+            if (article.slug === slug) {
+                setArticleActive(article);
+                return article;
+            }
+        });
+    }, [articleActive, slug, articles, setArticles]);
 
-      }
-      articles.forEach(article => {
-         if(article.slug === slug) {
-            setArticleActive(article);
-            return article;
-         }
-      })
-   }, [articleActive, slug, articles, setArticles])
-
-   console.log(articleActive)
-   // console.log(id);
-   // const articles = useArticles();
-	// const {
-	// 	data: articles,
-	// 	isPending,
-	// 	isFailed,
-	// } = useFetchArticles(`http://103.139.244.67/articles/${id}`);
-	return (
-		<>
-			{/* {isFailed && <div>{isFailed}</div>}
+    console.log(articleActive);
+    // console.log(id);
+    // const articles = useArticles();
+    // const {
+    // 	data: articles,
+    // 	isPending,
+    // 	isFailed,
+    // } = useFetchArticles(`http://103.139.244.67/articles/${id}`);
+    return (
+        <>
+            {/* {isFailed && <div>{isFailed}</div>}
 			{isPending && <div>Loading...</div>} */}
-			{articleActive !== null ? (
-               <InfoTemplate
-                  title={`${articleActive.title}`}
-                  props={
-                     'bg-whiteSoft xs:p-5 sm:p-5 p-20 mt-40 drop-shadow-xl mb-32'
-                  }
-               >
-                  <div>
-                     <Markdown>{articleActive.content}</Markdown>
-                  </div>
-               </InfoTemplate>
-         ) : (
-            console.log(articleActive, 'njjjj')
-         )}
-		</>
-	);
+            {articleActive !== null ? (
+                <InfoTemplate
+                    title={`${articleActive.subtitle}`}
+                    props={
+                        "bg-whiteSoft xs:p-5 sm:p-5 p-20 mt-40 drop-shadow-xl mb-32"
+                    }
+                >
+                    <div>
+                        <Markdown>{articleActive.content}</Markdown>
+                    </div>
+                </InfoTemplate>
+            ) : (
+                <InfoTemplate
+                    title={`Loading data...`}
+                    props={
+                        "bg-whiteSoft xs:p-5 sm:p-5 p-20 mt-40 drop-shadow-xl mb-32"
+                    }
+                >
+                    <div>loading</div>
+                </InfoTemplate>
+            )}
+        </>
+    );
 };
 
 export default FetchInfo;
