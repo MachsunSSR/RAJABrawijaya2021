16fa4c2122cbd3e94d38416e6478fcaf6bd74906
galihpermana29 <galihcbn123@gmail.com>
2021-08-11 20:51:57 +0700

    [galih] public folder, gladi revision

--- Frontend/src/routes/FetchArticles.jsx
+++ Frontend/src/routes/FetchArticles.jsx
@@ -8,7 +8,7 @@ export const getArticles = async () => {
 	return [
 		{
 			id: 1,
-			title: 'Pengumuman Gladi Bersih PKKMB RAJA Brawijaya',
+			title: 'Pengumuman Gladi Bersih',
 			slug: 'pengumuman-gladi-bersih',
 			content:
 				'Mahasiswa Baru Universitas Brawijaya 2020 WAJIB untuk menghadiri gladi bersih PKKMB 2021 yang diadakan pada :\n&nbsp;\n\n\n\n\n&nbsp;\n\n##### Tanggal  : Senin, 16 Agustus 2021\n##### Waktu   : 12.00 - 17.00 WIB\n##### Tempat  : Daring menggunakan Zoom (Link akan diperbarui pada website ini)\n&nbsp;\n\n\n\nDengan ketentuan pakaian saat gladi sebagai berikut :\n&nbsp;\n\n\n• Pakaian bebas dan rapi menggunakan atasan batik\n&nbsp;\n\n• Laki Laki celana panjang bawahan gelap \n&nbsp;\n\n• Peremuan rok bawahan gelap dengan jilbab hitam untuk yang berjilbab\n\n',
